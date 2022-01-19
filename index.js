e72a5d55a6da39f8d7b87ae44af3c060389c0b5e
Daeyoung <daeyoungchoi@hotmail.com>
2022-01-18 18:05:11 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+const main=document.getElementById("main");
+
+main.remove();
+
+
+const newHeader=document.createElement("h1");
+newHeader.textContent="YOUR-NAME is the champion";
+newHeader.id="victory";
+
+document.body.append(newHeader);
+
+
+
+
+
